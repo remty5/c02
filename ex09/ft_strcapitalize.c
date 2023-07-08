fc4703ec7894e53ef55604b1bbb4e04198fef90d
rvandepu <remty516@gmail.com>
2023-07-08 17:46:47 +0200

    ex09: commented out main + fixes for compiler flags

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,11 +6,9 @@
 /*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 10:16:06 by event             #+#    #+#             */
-/*   Updated: 2023/07/08 13:43:07 by event            ###   ########.fr       */
+/*   Updated: 2023/07/08 17:43:24 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 char	*ft_strcapitalize(char *str)
 {
@@ -30,14 +28,17 @@ char	*ft_strcapitalize(char *str)
 			c = 0;
 		else if (!c && 'A' <= str[i] && str[i] <= 'Z')
 			str[i] += 32;
-		else if (str[i] < 'A' || 'Z' < str[i] && str[i] < 'a' || 'z' < str[i])
+		else if (str[i] < 'A' || ('Z' < str[i] && str[i] < 'a') || 'z' < str[i])
 			c = 1;
 		i++;
 	}
 	return (str);
 }
 
-int main(int argc, char *argv[])
+/*#include <stdio.h>
+
+int	main(int argc, char *argv[])
 {
-	printf("%s\n", ft_strcapitalize(argv[1]));
-}
+	if (argc == 2)
+		printf("%s\n", ft_strcapitalize(argv[1]));
+}*/
